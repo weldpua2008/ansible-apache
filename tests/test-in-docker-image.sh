b7d0481af58fd5c63a0fc5fc0de4365f96af56d1
Valery Solovyov <weldpua2008@gmail.com>
2015-09-11 22:22:44 +0400

    src file does not exist

--- tests/test-in-docker-image.sh
+++ tests/test-in-docker-image.sh
@@ -9,6 +9,9 @@ OS_VERSION=${2:-}
 ANSIBLE_VERSION=${3:-}
 
 ANSIBLE_VAR=""
+ANSIBLE_INVENTORY="tests/test-inventory"
+ANSIBLE_PLAYBOOk="tests/test.yml"
+ANSIBLE_LOG_LEVEL="-vvv"
 
 # if there wasn't sudo then ansible couldn't use it
 if [ "x$SUDO" == "x" ];then
@@ -21,15 +24,39 @@ fi
 #    fi
 #
 #fi
-ANSIBLE_EXTRA_VARS=" -e \"${ANSIBLE_VAR}\" "
+ANSIBLE_EXTRA_VARS=""
+if [ "${ANSIBLE_VAR}x" == "x" ];then
+    ANSIBLE_EXTRA_VARS=" -e \"${ANSIBLE_VAR}\" "
+fi
+
 
 cd $RDIR/..
-
 printf "[defaults]\nroles_path = ../" > ansible.cfg
-ansible-playbook -i tests/test-inventory tests/test.yml --syntax-check
-#ANSIBLE_SHORT_VERSION=`ansible-playbook --version 2> /dev/null|cut -d " " -f2|cut -d "." -f1,2`
-ansible-playbook -i tests/test-inventory tests/test.yml -vvv --connection=local ${SUDO_OPTION} ${ANSIBLE_EXTRA_VARS}
 
-# Run the role/playbook again, checking to make sure it's idempotent.
-ansible-playbook -i tests/test-inventory tests/test.yml -vvv --connection=local ${SUDO_OPTION} ${ANSIBLE_EXTRA_VARS} | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
+function test_playbook_syntax(){
+
+    ansible-playbook -i ${ANSIBLE_INVENTORY} ${ANSIBLE_PLAYBOOk} --syntax-check || (echo "ansible playbook syntax check was failed" && exit 2 )
+}
+
+function test_playbook(){
+    # first run
+    ansible-playbook -i ${ANSIBLE_INVENTORY} ${ANSIBLE_PLAYBOOk} ${ANSIBLE_LOG_LEVEL} --connection=local ${SUDO_OPTION} ${ANSIBLE_EXTRA_VARS} || ( echo "first run was failed" && exit 2 )
+
+    # Run the role/playbook again, checking to make sure it's idempotent.
+    ansible-playbook -i ${ANSIBLE_INVENTORY} ${ANSIBLE_PLAYBOOk} ${ANSIBLE_LOG_LEVEL} --connection=local ${SUDO_OPTION} ${ANSIBLE_EXTRA_VARS} | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' ) || (echo 'Idempotence test: fail' && exit 1)
+}
+function extra_tests(){
+    php --version|| (echo "php --version was failed" && exit 100 )
+}
+
+function main(){
+    test_playbook_syntax
+    test_playbook
+    extra_tests
+
+}
+
+################ run #########################
+main
+exit 0
 
